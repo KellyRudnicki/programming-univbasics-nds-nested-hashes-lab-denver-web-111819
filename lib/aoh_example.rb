b7e2cdc24e755a98a47f098c2f83bc6e1911d585
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-09 19:25:08 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,7 +13,8 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G].sort
+  array = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  array.sort
 end
 
 def aoh_lookup(aoh, row, key)
